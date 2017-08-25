064528b77072e607021685420b00fbfa23556d01
Scarabol <scarabol@gmail.com>
2017-08-25 14:24:25 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Colony Survival SpawnProtect Mod by Scarabol
+
+This mods adds spawn protection and banner protection to your server. Griefers can not add or remove any blocks in a configurable area around your spawn. Furthermore a player can not build in the range of another players banner.
+
+## Configuration
+
+The mod directory contains a json file (**protection-ranges.json**) to configure the mod protection ranges on x and z axis. The y axis (height/depth) is not protected at all.
+
+## Permissions
+
+*mods.scarabol.spawnprotect*
+
+* super-permission, grants **all** permissions in this mod
+
+*mods.scarabol.spawnprotect.spawnchange*
+
+* grants permission to change blocks in the spawn range
+* you can use */spawnprotect spawn [steamid]* to grant this permission to a player, **which has played on this server before**
+* to revoke the permission, enter */spawnprotect nospawn [steamid]*
+
+*mods.scarabol.spawnprotect.banner.[steamid]*
+
+* grants permission for its owner, to change the blocks in the banner range of another player [steamid]
+* **everybody** can grant and revoke permissions for personal banner range, with */spawnprotect grant [steamid]* and */spawnprotect deny [steamid]*
+
+For example you want to grant permission to your friend (Steam-ID: 123456789) to change blocks in your banner range. Just enter **/spawnprotect grant 123456789** in the chat. Then ask your friend to do the same with your Steam-ID...
+
+## Installation
+
+**This mod must be installed on the server side!** No client installation required.
+
+* download a (compatible) [release](https://github.com/Scarabol/ColonySpawnProtectMod/releases) or build from source code (see below)
+* place the unzipped *Scarabol* folder inside your *ColonySurvival/gamedata/mods/* directory, like *ColonySurvival/gamedata/mods/Scarabol/*
+
+## Build
+
+* install Linux
+* download source code
+```Shell
+git clone https://github.com/Scarabol/ColonySpawnProtectMod
+```
+* use make
+```Shell
+cd ColonyConstructionMod
+make
+```
+
+**Pull requests welcome!**
+
---
+++ src/SpawnProtect.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Pipliz;
+using Pipliz.Chatting;
+using Pipliz.JSON;
+using Pipliz.Threading;
+using Permissions;
+using NPC;
+
+namespace ScarabolMods
+{
+  [ModLoader.ModManager]
+  public static class SpawnProtectModEntries
+  {
+    public static string PERMISSION_SUPER = "mods.scarabol.spawnprotect";
+    public static string PERMISSION_SPAWN_CHANGE = PERMISSION_SUPER + ".spawnchange";
+    public static string PERMISSION_BANNER_PREFIX = PERMISSION_SUPER + ".banner.";
+    private static int SpawnProtectionRangeX = 50;
+    private static int SpawnProtectionRangeZ = 50;
+    private static int BannerProtectionRangeX = 50;
+    private static int BannerProtectionRangeZ = 50;
+
+    [ModLoader.ModCallback(ModLoader.EModCallbackType.OnAssemblyLoaded, "scarabol.spawnprotect.assemblyload")]
+    public static void OnAssemblyLoaded(string path)
+    {
+      JSONNode jsonConfig;
+      if (Pipliz.JSON.JSON.Deserialize(Path.Combine(Path.GetDirectoryName(path), "protection-ranges.json"), out jsonConfig, false)) {
+        if (!jsonConfig.TryGetAs<int>("SpawnProtectionRangeX", out SpawnProtectionRangeX)) {
+          Pipliz.Log.Write(string.Format("Could not get spawn protection x-range from json config, using default value {0}", SpawnProtectionRangeX));
+        }
+        if (!jsonConfig.TryGetAs<int>("SpawnProtectionRangeZ", out SpawnProtectionRangeZ)) {
+          Pipliz.Log.Write(string.Format("Could not get spawn protection z-range from json config, using default value {0}", SpawnProtectionRangeZ));
+        }
+        if (!jsonConfig.TryGetAs<int>("BannerProtectionRangeX", out BannerProtectionRangeX)) {
+          Pipliz.Log.Write(string.Format("Could not get banner protection x-range from json config, using default value {0}", BannerProtectionRangeX));
+        }
+        if (!jsonConfig.TryGetAs<int>("BannerProtectionRangeZ", out BannerProtectionRangeZ)) {
+          Pipliz.Log.Write(string.Format("Could not get banner protection z-range from json config, using default value {0}", BannerProtectionRangeZ));
+        }
+      } else {
+        Pipliz.Log.Write("Could not find protection-ranges.json file");
+      }
+      Pipliz.Log.Write(string.Format("Using spawn protection with x-range {0}", SpawnProtectionRangeX));
+      Pipliz.Log.Write(string.Format("Using spawn protection with z-range {0}", SpawnProtectionRangeZ));
+      Pipliz.Log.Write(string.Format("Using banner protection with x-range {0}", BannerProtectionRangeX));
+      Pipliz.Log.Write(string.Format("Using banner protection with z-range {0}", BannerProtectionRangeZ));
+    }
+
+    [ModLoader.ModCallback(ModLoader.EModCallbackType.AfterStartup, "scarabol.spawnprotect.registercallbacks")]
+    public static void AfterStartup()
+    {
+      Pipliz.Log.Write("Loaded SpawnProtect Mod 0.9 by Scarabol");
+    }
+
+    [ModLoader.ModCallback(ModLoader.EModCallbackType.OnTryChangeBlockUser, "scarabol.spawnprotect.trychangeblock")]
+    public static bool OnTryChangeBlockUser(ModLoader.OnTryChangeBlockUserData d) {
+      Players.Player requestedBy = d.requestedBy;
+      Vector3Int position = d.VoxelToChange;
+      Vector3Int spawn = TerrainGenerator.GetSpawnLocation();
+      if (System.Math.Abs(spawn.x - position.x) <= SpawnProtectionRangeX && System.Math.Abs(spawn.z - position.z) <= SpawnProtectionRangeZ) {
+        if (!PermissionsManager.HasPermission(requestedBy, PERMISSION_SPAWN_CHANGE)) {
+          Chat.Send(requestedBy, "<color=red>You don't have permission to change the spawn area!</color>");
+          // TODO add counter and report to admins
+          return false;
+        }
+      } else {
+        int checkRangeX = BannerProtectionRangeX;
+        int checkRangeZ = BannerProtectionRangeZ;
+        if (d.typeToBuild == ItemTypes.IndexLookup.GetIndex("banner")) {
+          checkRangeX *= 2;
+          checkRangeZ *= 2;
+        }
+        foreach (Banner b in BannerTracker.GetBanners()) {
+          Vector3Int bannerLocation = b.KeyLocation;
+          if (System.Math.Abs(bannerLocation.x - position.x) <= checkRangeX && System.Math.Abs(bannerLocation.z - position.z) <= checkRangeZ) {
+            if (b.Owner != requestedBy && !PermissionsManager.HasPermission(requestedBy, PERMISSION_BANNER_PREFIX + b.Owner.ID.steamID)) {
+              Chat.Send(requestedBy, "<color=red>You don't have permission to change this area!</color>");
+              return false;
+            }
+            break;
+          }
+        }
+      }
+      return true;
+    }
+
+    [ModLoader.ModCallback(ModLoader.EModCallbackType.AfterItemTypesServer, "scarabol.spawnprotect.registertypes</color>")]
+    public static void AfterItemTypesServer()
+    {
+      ChatCommands.CommandManager.RegisterCommand(new SpawnProtectChatCommand());
+    }
+  }
+
+  public class SpawnProtectChatCommand : ChatCommands.IChatCommand
+  {
+    public bool IsCommand(string chat)
+    {
+      return chat.StartsWith("/spawnprotect ");
+    }
+
+    public bool TryDoCommand(Players.Player causedBy, string chattext)
+    {
+      var matched = Regex.Match(chattext, @"/spawnprotect (?<access>.+) (?<steamid>.+)");
+      if (!matched.Success) {
+        Chat.Send(causedBy, "Command didn't match, use /spawnprotect [spawn|nospawn|banner|deny] steamid");
+        return true;
+      }
+      string access = matched.Groups["access"].Value;
+      ulong steamid;
+      if (!ulong.TryParse(matched.Groups["steamid"].Value, out steamid)) {
+        Chat.Send(causedBy, "Failed parsing steamid");
+        return true;
+      }
+      Steamworks.CSteamID csteamid = new Steamworks.CSteamID(steamid);
+      if (!csteamid.IsValid()) {
+        Chat.Send(causedBy, "steamid is not valid");
+        return true;
+      }
+      NetworkID networkId = new NetworkID(csteamid);
+      Players.Player targetPlayer;
+      if (!Players.TryGetPlayer(networkId, out targetPlayer)) {
+        Chat.Send(causedBy, "Player not found or offline");
+        return true;
+      }
+      if (access.Equals("spawn")) {
+        if (!PermissionsManager.CheckAndWarnPermission(causedBy, SpawnProtectModEntries.PERMISSION_SUPER)) {
+          return true;
+        }
+        PermissionsManager.AddPermissionToUser(causedBy, targetPlayer, SpawnProtectModEntries.PERMISSION_SPAWN_CHANGE);
+      } else if (access.Equals("nospawn")) {
+        if (PermissionsManager.HasPermission(causedBy, SpawnProtectModEntries.PERMISSION_SUPER)) {
+          PermissionsManager.RemovePermissionOfUser(causedBy, targetPlayer, SpawnProtectModEntries.PERMISSION_SPAWN_CHANGE);
+        }
+      } else if (access.Equals("banner")) {
+        PermissionsManager.AddPermissionToUser(causedBy, targetPlayer, SpawnProtectModEntries.PERMISSION_BANNER_PREFIX + causedBy.ID.steamID);
+      } else if (access.Equals("deny")) {
+        PermissionsManager.RemovePermissionOfUser(causedBy, targetPlayer, SpawnProtectModEntries.PERMISSION_BANNER_PREFIX + causedBy.ID.steamID);
+      } else {
+        Chat.Send(causedBy, "Unknown access level, use /spawnprotect [spawn|nospawn|banner|deny] steamid");
+      }
+      return true;
+    }
+  }
+}
